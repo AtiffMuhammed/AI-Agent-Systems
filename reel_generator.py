ec691cc4c48841d93bf8f98b3993b8f9959525ea
Atiff Muhammed <athifae491@gmail.com>
2026-04-23 19:07:06 +0800

    Create reel_generator.py

---
+++ reel_generator.py
@@ -0,0 +1,14 @@
+# Simple Reel Idea Generator
+
+ideas = [
+    "Did you know facts",
+    "Life hacks",
+    "Motivation short",
+    "Funny moment",
+    "AI facts"
+]
+
+import random
+
+print("Your next reel idea:")
+print(random.choice(ideas))
