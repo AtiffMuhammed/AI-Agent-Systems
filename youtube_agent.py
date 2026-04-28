5690185bf9cffc33ddaf14922d15e29323f0eb35
Atiff Muhammed <athifae491@gmail.com>
2026-04-28 19:40:25 +0800

    Delete youtube_agent.py

--- youtube_agent.py
+++
@@ -1,7 +0,0 @@
-# Simple AI YouTube Title Generator
-
-def generate_title(topic):
-    return f"🔥 Amazing Facts About {topic} You Didn't Know!"
-
-topic = input("Enter video topic: ")
-print(generate_title(topic))
